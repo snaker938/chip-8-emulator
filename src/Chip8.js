fd196995508401a8a91c9be1f2fb8d0acbb104b8
snaker938 <jmendel123@gmail.com>
2021-09-02 01:15:50 +0100

    Added registers

--- src/Chip8.js
+++ src/Chip8.js
@@ -1,10 +1,12 @@
 import { Display } from "./Display";
 import { Memory } from "./Memory";
+import { Registers } from "./Registers";
 
 export class Chip8 {
   constructor() {
     console.log("Create a new Chip-8");
     this.display = new Display();
     this.memory = new Memory();
+    this.registers = new Registers();
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import { Chip8 } from "./Chip8";
 
 const chip8 = new Chip8();
-chip8.memory.setMemory(0x05, 0x1a);
-const result = chip8.memory.getMemory(0x05);
+
+chip8.registers.stackPush(1);
+chip8.registers.stackPush(2);
+chip8.registers.stackPush(3);
+
+let result;
+
+result = chip8.registers.stackPop();
+console.log(result);
+
+result = chip8.registers.stackPop();
+console.log(result);
+
+result = chip8.registers.stackPop();
 console.log(result);
